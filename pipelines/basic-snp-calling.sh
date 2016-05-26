ed5d02779be0c2c4522632e2b0dbb9a9fe53cfb6
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-05-26 16:52:18 -0400

    Changes to run khmer filtering

--- pipelines/basic-snp-calling.sh
+++ pipelines/basic-snp-calling.sh
@@ -12,12 +12,28 @@
 THREADS=2
 MEMORYGB=4
 TMPDIR=/tmp
+
+# Toggle comments to change species..
+
+SPECIES='Solenopsis invicta'
+REFERENCE=244018/GCA_000188075.1_Si_gnG_genomic.fna.gz
+READS=2209130
+READSFQ=DRR042176
+
 SPECIES='Guillardia theta'
 REFERENCE=503988/GCA_000315625.1_Guith1_genomic.fna.gz
 READS=35526
 
+# Salmonella enterica
+# SPECIES='GCA_000006945.2'
+REFERENCE='GCA_000006945.2_ASM694v2_genomic.fna.gz'
+READS='2492428'
+READSFQ='ERR1229296'
+
 # Download reference genome
-bionode-ncbi download assembly $SPECIES
+# bionode-ncbi download assembly $SPECIES
+# TODO not curl..
+curl -O ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA_000006945.2_ASM694v2/$REFERENCE
 
 # Download sequencing reads
 bionode-ncbi download sra $READS
@@ -30,47 +46,59 @@ fastq-dump --split-files --skip-technical --gzip $READS/**.sra
 # for single end or other technologies (e.g., change the adapters/TruSeq3-PE.fa file)
 # Adapter files are included with Trimmomatic installation.
 # Parameters from quick start guide: http://www.usadellab.org/cms/?page=trimmomatic
-trimmomatic PE -threads $THREADS -phred33 \
-  $READS\_1.fastq.gz $READS\_2.fastq.gz \
-  $READS\_1.trim.pe.fastq.gz $READS\_1.trim.se.fastq.gz \
-  $READS\_2.trim.pe.fastq.gz $READS\_2.trim.se.fastq.gz \
+
+# java -jar trimmomatic-0.36.jar PE -threads $THREADS -phred33 \
+java -jar trimmomatic-0.36.jar PE -phred33 \
+  $READSFQ\_1.fastq.gz $READSFQ\_2.fastq.gz \
+  $READSFQ\_1.trim.pe.fastq.gz $READSFQ\_1.trim.se.fastq.gz \
+  $READSFQ\_2.trim.pe.fastq.gz $READSFQ\_2.trim.se.fastq.gz \
   ILLUMINACLIP:adapters/TruSeq3-PE.fa:2:30:10 \
   LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:36
+
+# For Single end, hardcoded to Guillardia theta
+# java -jar trimmomatic-0.36.jar SE -phred33 \
+#   SRR070675_2.fastq.gz trim.SRR070675_2.fastq.gz \
+#   ILLUMINACLIP:adapters/TruSeq3-SE.fa:2:30:10 \
+#   LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:36
 
 # Filter reads with low-abundance k-mers using khmer
 # settings (will need to be adjusted)
 KMERSIZE=20
 PLOTXMAX=60
-PLOTYMAX=12000000
+PLOTYMAX=1200000
 # merge paired end fasta files into interleaved fasta
-seqtk mergepe $READS\_1.trim.pe.fastq.gz $READS\_2.trim.pe.fastq.gz | gzip > $READS.trim.pe.fastq.gz
+seqtk mergepe $READSFQ\_1.trim.pe.fastq.gz $READSFQ\_2.trim.pe.fastq.gz | gzip > $READSFQ.trim.pe.fastq.gz
 # count k-mers
-load-into-counting.py -N 4 -k $KMERSIZE -x 8e9 -T $THREADS $READS.trim.pe.fastq.gz.kh  $READS.trim.pe.fastq.gz
+# 25mins
+load-into-counting.py -N 4 -k $KMERSIZE -M 8e9 -T $THREADS $READSFQ.trim.pe.fastq.gz.kh  $READSFQ.trim.pe.fastq.gz
 # create histogram of counts
-abundance-dist.py $READS.trim.pe.fastq.gz.kh $READS.trim.pe.fastq.gz $READS.trim.pe.fastq.gz.kh.hist
+abundance-dist.py $READSFQ.trim.pe.fastq.gz.kh $READSFQ.trim.pe.fastq.gz $READSFQ.trim.pe.fastq.gz.kh.hist
 # plot histogram (xmax and ymax might need adjustments so that plot can be interpreted)
-plot-abundance-dist.py --xmax $PLOTXMAX --ymax $PLOTYMAX $READS.trim.pe.fastq.gz.kh.hist $READS.trim.pe.fastq.gz.kh.hist.png
+# plot-abundance-dist.py --xmax $PLOTXMAX --ymax $PLOTYMAX $READSFQ.trim.pe.fastq.gz.kh.hist $READSFQ.trim.pe.fastq.gz.kh.hist.png
+./plotKhmer.R $READSFQ.trim.pe.fastq.gz.kh.hist $READSFQ.trim.pe.fastq.gz.kh.hist.png $PLOTXMAX $PLOTYMAX
+
 # INTERACTION REQUIRED: from plot results, pick a minimum frequency cutoff for filter
-MINCOVERAGE=8
+MINCOVERAGE=5
 # filter low-abundance k-mers from reads (reads are not entirely discarded)
-filter-abund.py -T $THREADS -C $MINCOVERAGE $READS.trim.pe.fastq.gz.kh -o $READS.trim.pe.khmer.fastq.gz $READS.trim.pe.fastq.gz
+# 14mins
+filter-abund.py -T $THREADS -C $MINCOVERAGE $READSFQ.trim.pe.fastq.gz.kh -o $READSFQ.trim.pe.khmer.fastq.gz $READSFQ.trim.pe.fastq.gz
 
 # Faster alternative k-mer filtering using refresh-bio/KMC
 # this methods discards reads so in a paired end dataset it might break some pairs
 # these orphan reads might need to be removed otherwise this could cause issues downstream
 # count k-mers
-kmc -k$KMERSIZE -m$MEMORYGB -t$THREADS $READS.trim.pe.fastq.gz $READS.trim.pe.kmc .
+kmc -k$KMERSIZE -m$MEMORYGB -t$THREADS $READSFQ.trim.pe.fastq.gz $READSFQ.trim.pe.kmc .
 # make histogram
-kmc_tools histogram $READS.trim.pe.kmc $READS.trim.pe.kmc.hist.txt
+kmc_tools histogram $READSFQ.trim.pe.kmc $READSFQ.trim.pe.kmc.hist.txt
 # plot histogram (R script not included with kmc)
-plotKMC.R $READS.trim.pe.kmc.hist.txt $READS.trim.pe.kmc.hist.png $PLOTXMAX $PLOTYMAX
+plotKMC.R $READSFQ.trim.pe.kmc.hist.txt $READSFQ.trim.pe.kmc.hist.png $PLOTXMAX $PLOTYMAX
 # INTERACTION REQUIRED: from plots results, pick a minimum frequency cutoff for filter
 MINCOVERAGE=8
 # Filter rare k-mers in one step (slower but uses less disk space)
-kmc_tools filter $READS.trim.pe.kmc -cx$MINCOVERAGE $READS.trim.pe.fastq.gz -ci0 -cx0 $READS.trim.pe.kmc.fastq.gz
+kmc_tools filter $READSFQ.trim.pe.kmc -cx$MINCOVERAGE $READSFQ.trim.pe.fastq.gz -ci0 -cx0 $READSFQ.trim.pe.kmc.fastq.gz
 # Or, filter in two steps (faster but requires more disk space)
-kmc_tools reduce $READS.trim.pe.kmc  -cx$MINCOVERAGE $READS.rare_k-mers.kmc
-kmc_tools filter $READS.rare_k-mers.kmc $READS.trim.pe.fastq.gz -ci0 -cx0 $READS.trim.pe.kmc.fastq.gz
+kmc_tools reduce $READSFQ.trim.pe.kmc  -cx$MINCOVERAGE $READSFQ.rare_k-mers.kmc
+kmc_tools filter $READSFQ.rare_k-mers.kmc $READSFQ.trim.pe.fastq.gz -ci0 -cx0 $READSFQ.trim.pe.kmc.fastq.gz
 # The previous steps might require some explanation. We are filtering from the kmc k-mers table all k-mers
 # above our limit with the first -cx (so keeping the rare ones, this is counter intuitive). Then, we remove
 # from the fastq file all the reads that have a k-mer present in the filtered table, i.e., a rare k-mer (the -ci0 and -cx0).
